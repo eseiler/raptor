ef78cd7da38baefaf6ae6d9017f174e730858876
Enrico Seiler <enrico.seiler@hotmail.de>
2024-07-08 11:51:35 +0200

    Update

--- util/partitioned/config.sh
+++ util/partitioned/config.sh
@@ -8,7 +8,8 @@ set -euo pipefail
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     echo "The script '$(readlink -f $0)' must be sourced, not executed."
-    echo "Please run 'source $(readlink -f $0)' instead."
+    echo "There is potentially another script that sources this script."
+    echo "If not, please run 'source $(readlink -f $0)' instead."
     exit 1
 fi
 
@@ -22,9 +23,9 @@ OUTPUT_DIR="/srv/data/seiler/part_simulation"
 # The values must not contain spaces, i.e., "4 G" is not allowed.
 # The suffixes are interpreted as binary suffixes, i.e., 1K = 1024.
 REFERENCE_LENGTH=128M
-NUMBER_OF_PARTS="64 512 1024"
+NUMBER_OF_PARTS="64 512 1024 655360"
 NUMBER_OF_HAPLOTYPES=16
-READ_COUNT=1M
+READ_COUNT=10M
 # The total sequence size will be REFERENCE_LENGTH * NUMBER_OF_HAPLOTYPES
 
 ###
@@ -51,6 +52,32 @@ to_iec() {
     numfmt --to=iec $1 --format '%.0f'
 }
 
+check_config() {
+    if [[ ${REFERENCE_LENGTH} -lt ${READ_LENGTH} ]]; then
+        echo "The reference length (${REFERENCE_LENGTH}) must be greater than the read length (${READ_LENGTH})."
+        return 1
+    fi
+
+    if [[ ${READ_ERRORS} -gt ${READ_LENGTH} ]]; then
+        echo "The number of read errors (${READ_ERRORS}) must be less than the read length (${READ_LENGTH})."
+        return 1
+    fi
+
+    for PART in ${NUMBER_OF_PARTS}; do
+        if [[ ${PART} -gt ${READ_COUNT} ]]; then
+            echo "The number of parts (${PART}) must be less than the number of reads (${READ_COUNT})."
+            return 1
+        fi
+
+        if [[ $((${READ_COUNT} % ${PART})) -ne 0 ]]; then
+            echo "The number of reads (${READ_COUNT}) must be divisible by the number of parts (${PART})."
+            return 1
+        fi
+    done
+}
+
 expand_suffix "REFERENCE_LENGTH"
 expand_suffix "READ_COUNT"
 expand_suffix "NUMBER_OF_HAPLOTYPES"
+
+check_config
