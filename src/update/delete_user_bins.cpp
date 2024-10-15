4555f9a973f7dd96aa74e2472f6dff290b1c035e
Enrico Seiler <enrico.seiler@hotmail.de>
2024-10-15 17:55:01 +0200

    [FEATURE] Dynamic

---
+++ src/update/delete_user_bins.cpp
@@ -0,0 +1,102 @@
+// SPDX-FileCopyrightText: 2006-2024 Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2024 Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: BSD-3-Clause
+
+/*!\file
+ * \brief Implements raptor::delete_user_bins.
+ * \author Enrico Seiler <enrico.seiler AT fu-berlin.de>
+ */
+
+#include <raptor/update/delete_user_bins.hpp>
+
+namespace raptor
+{
+
+void delete_user_bins(update_arguments const & arguments, raptor_index<index_structure::hibf> & index)
+{
+    std::cerr << "\nDeleting user bins: ";
+    for (auto const bin : arguments.user_bins_to_delete)
+        std::cerr << bin << ' ';
+    std::cerr << '\n';
+
+    auto & ibf_bin_to_user_bin_id = index.ibf().ibf_bin_to_user_bin_id;
+    // auto & next_ibf_id = index.ibf().next_ibf_id;
+    std::vector<seqan::hibf::bin_index> technical_bins_to_delete{};
+
+    for (size_t ibf_index = 0; ibf_index < ibf_bin_to_user_bin_id.size(); ++ibf_index)
+    {
+        technical_bins_to_delete.clear();
+
+        auto & to_user_bin_id = ibf_bin_to_user_bin_id[ibf_index];
+        auto & ibf = index.ibf().ibf_vector[ibf_index];
+
+        for (size_t bin_index = 0; bin_index < to_user_bin_id.size(); ++bin_index)
+        {
+            uint64_t & user_bin_id = to_user_bin_id[bin_index];
+            // This also ensures that invalid user bin IDs are not processed. TODO: Warning/Check?
+            if (std::ranges::contains(arguments.user_bins_to_delete, user_bin_id))
+            {
+                technical_bins_to_delete.push_back(seqan::hibf::bin_index{bin_index});
+                user_bin_id = seqan::hibf::bin_kind::deleted;
+            }
+        }
+
+        if (!technical_bins_to_delete.empty())
+        {
+            ibf.clear(technical_bins_to_delete);
+            for (auto const technical_bin_index : technical_bins_to_delete)
+            {
+                ibf.occupancy[technical_bin_index.value] = 0u;
+                ibf.occupied_bins[technical_bin_index.value] = false;
+            }
+            [[maybe_unused]] bool const set_bin_count1 =
+                ibf.set_bin_count(seqan::hibf::bin_count{ibf.bin_count() - technical_bins_to_delete.size()});
+            assert(set_bin_count1); // reducing bin count should always work
+            // Either there is no occupied bin or there are still user bins. Both cannot be false or true at the same time.
+            assert(ibf.occupied_bins.none() ^ ibf.bin_count());
+            // Delete in parent
+            if (ibf_index != 0 && ibf.occupied_bins.none())
+            {
+                auto const parent = index.ibf().prev_ibf_id[ibf_index];
+
+                auto & parent_ibf = index.ibf().ibf_vector[parent.ibf_idx];
+                parent_ibf.clear(seqan::hibf::bin_index{parent.bin_idx});
+
+                parent_ibf.occupancy[parent.bin_idx] = 0u;
+                parent_ibf.occupied_bins[parent.bin_idx] = false;
+                ibf_bin_to_user_bin_id[parent.ibf_idx][parent.bin_idx] = seqan::hibf::bin_kind::deleted;
+                [[maybe_unused]] bool const set_bin_count2 =
+                    parent_ibf.set_bin_count(seqan::hibf::bin_count{parent_ibf.bin_count() - 1u});
+                assert(set_bin_count2); // reducing bin count should always work
+            }
+        }
+    }
+}
+
+// Delete in parent
+// if (ibf_index != 0 && ibf.occupied_bins.none())
+// {
+//     for (size_t parent_ibf_idx = 0; parent_ibf_idx < next_ibf_id.size(); ++parent_ibf_idx)
+//     {
+//         if (parent_ibf_idx == ibf_index)
+//             continue;
+
+//         auto & parent_next_ibf = next_ibf_id[parent_ibf_idx];
+
+//         for (size_t parent_bin_index = 0; parent_bin_index < parent_next_ibf.size(); ++parent_bin_index)
+//         {
+//             if (parent_next_ibf[parent_bin_index] == static_cast<int64_t>(ibf_index))
+//             {
+//                 auto & parent_ibf = index.ibf().ibf_vector[parent_ibf_idx];
+//                 parent_ibf.clear(seqan::hibf::bin_index{parent_bin_index});
+
+//                 parent_ibf.occupancy[parent_bin_index] = 0u;
+//                 parent_ibf.occupied_bins[parent_bin_index] = false;
+//                 ibf_bin_to_user_bin_id[parent_ibf_idx][parent_bin_index] = -2;
+//                 parent_ibf.overwrite_bin_count(parent_ibf.bin_count() - 1u);
+//             }
+//         }
+//     }
+// }
+
+} // namespace raptor
---
+++ src/update/dump_index.cpp
@@ -0,0 +1,74 @@
+// SPDX-FileCopyrightText: 2006-2024 Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2024 Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: BSD-3-Clause
+
+/*!\file
+ * \brief Implements raptor::dump_index.
+ * \author Enrico Seiler <enrico.seiler AT fu-berlin.de>
+ */
+
+#include <raptor/update/dump_index.hpp>
+
+namespace raptor
+{
+
+void dump_index(raptor_index<index_structure::hibf> const & index)
+{
+    dump_index(index.ibf());
+}
+
+void dump_index(seqan::hibf::hierarchical_interleaved_bloom_filter const & hibf)
+{
+    std::cerr << "\nDumping index\n";
+    // std::cerr << "Window size: " << index.window_size() << '\n';
+    // std::cerr << "Shape:       " << index.shape().to_string() << '\n';
+    // std::cerr << "Parts:       " << index.parts() << '\n';
+    // std::cerr << "FPR:         " << index.fpr() << '\n';
+    // std::cerr << "Type:        " << (index.is_hibf() ? "HIBF" : "IBF") << '\n';
+    // std::cerr << "Bin path:    " << index.bin_path().front().front() << '\n';
+    for (auto const & to_user_bin_id : hibf.ibf_bin_to_user_bin_id)
+    {
+        std::cerr << "User bin[" << to_user_bin_id.size() << "]:   [";
+        char sep{};
+        for (auto const val : to_user_bin_id)
+        {
+            switch (val)
+            {
+            case seqan::hibf::bin_kind::deleted:
+                std::cerr << sep << 'D';
+                break;
+            case seqan::hibf::bin_kind::merged:
+                std::cerr << sep << 'M';
+                break;
+            default:
+                std::cerr << sep << val;
+            }
+
+            sep = ',';
+        }
+        std::cerr << "]\n";
+    }
+    for (auto const & ibf : hibf.ibf_vector)
+    {
+        std::cerr << "Num bins:    " << ibf.bin_count() << '\n';
+        std::cerr << "  Occupancy[" << ibf.occupancy.size() << "]:   [";
+        char sep{};
+        for (auto const val : ibf.occupancy)
+        {
+            std::cerr << sep << val;
+            sep = ',';
+        }
+        std::cerr << "]\n";
+        std::cerr << "  Occupied[" << ibf.occupied_bins.size() << "]:    [";
+        sep = {};
+        for (auto const val : ibf.occupied_bins)
+        {
+            std::cerr << sep << val;
+            sep = ',';
+        }
+        std::cerr << "]\n";
+    }
+    std::cerr << std::flush;
+}
+
+} // namespace raptor
